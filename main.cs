186fcc5e5f30f09ea02526cb42aca82300ee8041
kubacz002 <kuba-projekt@wp.pl>
2022-05-28 20:42:05 +0200

    dodanie funkcji z informacją o niezadowoleniu z braku podania imienia

--- main.cs
+++ main.cs
@@ -7,13 +7,16 @@ class program
     	void showName(name){
     		console.writeLine("Witaj na pokładzie, " + name);
     	}
+        void errorName(){
+            console.writeLine("dlaczego nie chcesz się przedstawic ? Nie podoba ci się mój program ?");
+        }
         console.writeLine("witaj w kursie git, jak masz na imię ?");
         string name = console.readLine();
         if(name.length != 0){
             showName(name);
         }
         else{
-            console.writeLine("dlaczego nie chcesz się przedstawic ? Nie podoba ci się mój program ?");
+            errorName();
         }
     }
 }
