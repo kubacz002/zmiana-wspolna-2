662fbda81203dddc73fa0358ff8bffcb973e8fc5
Jakub Czarnecki <jczarnecki014@gmail.com>
2022-05-28 20:36:42 +0200

    dodanie funkcji z przywitaniem usera

--- main.cs
+++ main.cs
@@ -4,10 +4,13 @@ class program
 {
     public static void main(string args[])
     {
+    	void showName(name){
+    		console.writeLine("Witaj na pokładzie, " + name);
+    	}
         console.writeLine("witaj w kursie git, jak masz na imię ?");
         string name = console.readLine();
         if(name.length != 0){
-            console.writeLine("Witaj na pokładzie, " + name);
+            showName(name);
         }
         else{
             console.writeLine("dlaczego nie chcesz się przedstawic ? Nie podoba ci się mój program ?");
